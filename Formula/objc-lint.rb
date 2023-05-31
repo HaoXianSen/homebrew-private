8c74ebf41669d258b71444e7dd9c3c4e49e4d51a
郝玉鸿 <haoyh02@rd.netease.com>
2023-05-31 12:02:35 +0800

    修改formula脚本名字

--- Formula/objc-lint.rb
+++ Formula/objc-lint.rb
@@ -1,7 +1,7 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class ObjectiveCLint < Formula
+class ObjcLint < Formula
   desc "A Objective-C code lint tool"
   homepage "https://github.com/HaoXianSen/Objective-CLint"
   url "https://github.com/HaoXianSen/Objective-CLint/archive/refs/tags/v1.0.0.tar.gz"
