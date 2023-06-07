47ee3cbc27557f6d7b4619ea62b942883edf6190
郝玉鸿 <haoyh02@rd.netease.com>
2023-06-07 17:51:14 +0800

    升级lint-maker版本

--- Formula/lint-maker.rb
+++ Formula/lint-maker.rb
@@ -4,10 +4,10 @@
 class LintMaker < Formula
   desc "This is install objc and swift lint tool automatically"
   homepage "https://github.com/HaoXianSen/LintMaker"
-  url "https://github.com/HaoXianSen/LintMaker/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "1cc6a0e4f81d78174799ab9b9b0ca10460842bb9622644b794e83bd5df865153"
+  url "https://github.com/HaoXianSen/LintMaker/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "b06b99c40ba9539940a8f93edff0cce2b9449a352c8e247b81c258b041348644"
   license "MIT"
-  version "1.0.0"
+  version "1.0.1"
   head "https://github.com/HaoXianSen/LintMaker.git", branch: "main"
 
   depends_on xcode: ["14.0", :build]
